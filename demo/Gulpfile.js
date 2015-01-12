b94fa7a9e364a5e03d2bdd4efffd3094bdc683e0
adamzhou <adamzhou@ghy.cn>
2015-01-12 10:57:00 +0800

    修改

---
+++ demo/Gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+    include = require('gulp-include');
+gulp.task('default',function(){
+  gulp.src('*.html')
+      .pipe(include({
+        baseUrl:"../../sinclude/"
+      }))
+      .pipe(gulp.dest('dest/'));
+});
